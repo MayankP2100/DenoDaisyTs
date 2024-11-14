b24ef3fcb18ad72557f2aae167de58806d2a2aec
Mayank <74498800+MayankP2100@users.noreply.github.com>
2024-11-14 17:48:23 +0530

    Update tailwind.config.js
    Removed tailwindcss/typography module

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui";
-import * as twct from "@tailwindcss/typography";
 import autoprefixer from "autoprefixer";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [twct, daisyui],
+  plugins: [daisyui],
   daisyui: {
     themes: ["light", "dark", "retro", "cyberpunk", "valentine", "aqua"],
   },
